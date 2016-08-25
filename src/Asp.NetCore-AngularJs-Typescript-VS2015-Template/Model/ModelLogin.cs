b97d1306170f4d84bbafcd4033c31382fdc42f33
David Pires <davidblkx@outlook.com>
2016-08-25 15:32:22 +0100

    Add project files.

---
+++ src/Asp.NetCore-AngularJs-Typescript-VS2015-Template/Model/ModelLogin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore_AngularJs_Typescript_VS2015_Template.Model
+{
+    public class ModelLogin
+    {
+        public string User { get; set; }
+        public string Pass { get; set; }
+        public bool IsPersistent { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
